1675621a9e0513af781bd9c2666c26992e055367
RaffIncandenza <cuentafalsamelae@gmail.com>
2025-08-31 21:49:37 -0400

    Second Commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     # Define the URL for the emotion detection API
@@ -9,5 +10,27 @@ def emotion_detector(text_to_analyze):
     payload = { "raw_document": { "text": text_to_analyze } }
     # Make a POST request to the API
     response = requests.post(url, json=payload, headers=headers)
-    # Return the raw text of the response
-    return response.text
+    
+    # 1. Convert response text to a dictionary
+    formatted_response = json.loads(response.text)
+
+    # 2. Extract the required set of emotions and their scores
+    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = emotion_scores['anger']
+    disgust_score = emotion_scores['disgust']
+    fear_score = emotion_scores['fear']
+    joy_score = emotion_scores['joy']
+    sadness_score = emotion_scores['sadness']
+
+    # 3. Find the dominant emotion
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    # 4. Return the formatted output
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
